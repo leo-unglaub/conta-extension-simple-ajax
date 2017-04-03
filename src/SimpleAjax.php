f8cad6fe23ab5a7171256f40f64d7b1b402d792a
Leo Unglaub <leo@unglaub.at>
2017-04-03 22:42:31 +0200

    Add cleaned up versions of the existing files
    Signed-off-by: Leo Unglaub <leo@unglaub.at>

---
+++ src/SimpleAjax.php
@@ -0,0 +1,60 @@
+<?php
+
+define ('TL_MODE', 'FE');
+define ('BYPASS_TOKEN_CHECK', true);
+
+require ('system/initialize.php');
+
+
+/**
+ * Class SimpleAjax
+ */
+class SimpleAjax extends Controller
+{
+	/**
+	 * Call the parent constructor.
+	 *
+	 * !!! DON'T REMOVE THIS !!!
+	 *
+	 * If you remove this you get the following error message:
+	 * Fatal error: Call to protected System::__construct() from invalid
+	 * context
+	 *
+	 * @param	void
+	 * @return	void
+	 */
+	public function __construct ()
+	{
+		parent::__construct ();
+	}
+
+
+	/**
+	 * Get the ajax request and call all hooks
+	 *
+	 * @param	void
+	 * @return	void
+	 */
+	public function run ()
+	{
+		// check if a hook is registered
+		if (is_array ($GLOBALS['TL_HOOKS']['simpleAjax']) && count ($GLOBALS['TL_HOOKS']['simpleAjax']) > 0)
+		{
+			// execute every registered callback
+			foreach ($GLOBALS['TL_HOOKS']['simpleAjax'] as $callback)
+			{
+				$this->import ($callback[0]);
+				$this->$callback[0]->$callback[1] ();
+			}
+		}
+
+		// if there is no other output, we generate a 412 error response
+		header ('HTTP/1.1 412 Precondition Failed');
+		die ('Simple Ajax: Invalid AJAX call.');
+	}
+}
+
+
+// create a SimpleAjax instance and run it
+$objSimpleAjax = new SimpleAjax ();
+$objSimpleAjax->run ();
---
+++ src/SimpleAjaxFrontend.php
@@ -0,0 +1,75 @@
+<?php
+
+define ('TL_MODE', 'FE');
+define ('BYPASS_TOKEN_CHECK', true);
+
+require ('system/initialize.php');
+
+
+/**
+ * Class SimpleAjaxFrontend
+ */
+class SimpleAjaxFrontend extends Frontend
+{
+	/**
+	 * Set some important constants
+	 *
+	 * @param	void
+	 * @return	void
+	 */
+	public function __construct ()
+	{
+		$objFrontendUser = FrontendUser::getInstance ();
+
+		// call the constructor from Frontend
+		parent::__construct ();
+
+		// check if a user is logged in
+		define ('BE_USER_LOGGED_IN', $this->getLoginStatus ('BE_USER_AUTH'));
+		define ('FE_USER_LOGGED_IN', $this->getLoginStatus ('FE_USER_AUTH'));
+
+		// set static url's in case the user generated HTML code
+		\Controller::setStaticUrls ();
+	}
+
+
+	/**
+	 * Get the ajax request and call all hooks
+	 *
+	 * @param	void
+	 * @return	void
+	 */
+	public function run ()
+	{
+		// run the global hook
+		if (is_array ($GLOBALS['TL_HOOKS']['simpleAjax']) && count ($GLOBALS['TL_HOOKS']['simpleAjax']) > 0)
+		{
+			// execute every registered callback
+			foreach ($GLOBALS['TL_HOOKS']['simpleAjax'] as $callback)
+			{
+				$this->import ($callback[0]);
+				$this->$callback[0]->$callback[1] ();
+			}
+		}
+
+		// run the frontend exclusive hook
+		if (is_array ($GLOBALS['TL_HOOKS']['simpleAjaxFrontend']) && count ($GLOBALS['TL_HOOKS']['simpleAjaxFrontend']) > 0)
+		{
+			// execute every registered callback
+			foreach ($GLOBALS['TL_HOOKS']['simpleAjaxFrontend'] as $callback)
+			{
+				$this->import ($callback[0]);
+				$this->$callback[0]->$callback[1] ();
+			}
+		}
+
+		// if there is no other output, we generate a 412 error response
+		header ('HTTP/1.1 412 Precondition Failed');
+		die ('Simple Ajax: Invalid AJAX call.');
+	}
+}
+
+
+// create a SimpleAjax instance and run it
+$objSimpleAjaxFrontend = new SimpleAjaxFrontend();
+$objSimpleAjaxFrontend->run();
